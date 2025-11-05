df9f0589664ac6c83d53dbc265747f928d971c99
okey <netun9825@gmail.com>
2025-11-05 16:40:12 +0600

    lr4

--- alg_3/main.py
+++ alg_3/main.py
@@ -25,15 +25,116 @@ N K Z
 3 6 1
 3 4 2
 """
+from typing import final
+
+"""
+
+
 mN = int(input())
 
 tmp = 0
 z = 1
 while True:
     d = (2 ** z) - 1
-    if d > mN:
-        break
-    tmp += mN // d
-    z += 1
+
 
 print(tmp)
+"""
+
+
+# tmp = 0
+# for n in range(1, int(input()) + 1):
+#     if n == 1:
+#         print(n, n)
+#         tmp += 1
+#         continue
+#
+#     for k in range(n, (n * 2) + 1):
+#         m = (n * 2) / k
+#         if int(m) * k == n * 2:
+#             print(n, k, m)
+#             tmp += 1
+"""
+tmp = 0
+ww = 3
+for x in range(1, (ww * 2) + 1):
+    if x == 1:
+        tmp += 1
+        continue
+        
+    for y in range(2, x + 1):
+        if x % y == 0 and x != y:
+            print(x, y, )
+            tmp += 1
+
+print(tmp)
+"""
+
+# Матрица N * N,
+# 1. Определить номера столбцов с однинаковыми элементами
+"""
+import math
+
+def www(x, start):
+    a = []
+    tmp = None
+    for z, q in enumerate(x[start:]):
+        if tmp is None:
+            tmp = q
+        if tmp == q:
+            a.append(z + start)  
+    if len(a) != 1:
+        print(a)
+
+
+
+matrix = [[1, 2, 0, 2, 3],
+          [2, 2, 2, 2, 3],
+          [0, 2, 1, 2, 4],
+          [1, 2, 1, 2, 5]]
+
+columns = list(map(list, zip(*matrix)))
+
+cc = []
+for i in range(len(columns)):
+    cc.append([str(columns[i]).count('0'), sum(columns[i]), math.prod(columns[i])])
+
+for i in range(len(cc)):
+    www(cc, i)
+"""
+# 2. Определить положение элементов значение которых является минимальный строке и максимальным столбце на пересечение которых на пересечение которых они стоят (или наоборот)
+
+matrix = [[2, 1, 3],
+          [4, -5, 6],
+          [7, -8, 9]]
+
+matrix = [[1, 2, 1],
+          [6, 7, 7],
+          [2, 9, 1]]
+
+columns = list(map(list, zip(*matrix)))
+
+for i in range(len(matrix)):
+    for y in range(len(columns)):
+        if max(matrix[i]) == min(columns[y]):
+            print(i + 1, y + 1)
+
+
+
+# 3. Матрица квадратная, определить является ли она магическим квадратом
+"""
+matrix = [[1, 2, 3],
+          [3, 1, 2],
+          [2, 3, 1]]
+
+columns = list(map(list, zip(*matrix)))
+
+for i in range(len(matrix)):
+    if sum(matrix[i]) == sum(columns[i]):
+        continue
+    else:
+        print("Не равно")
+        exit(0)
+
+print("Равно")
+"""
